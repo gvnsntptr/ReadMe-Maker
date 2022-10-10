7c3b1900a9a5361248c3dadb26d868f3eee51e51
Gavin <gsantopetro.contact@gmail.com>
2022-10-10 13:37:55 -0500

    make .md files with index.js, blank spaces in generateMarkdown

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,21 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    fs.appendFile('log.md', `${process.argv[2]}\n`, (err) =>
+    // TODO: Describe how this ternary operator works
+    err ? console.error(err) : console.log('Commit logged!')
+    );
+
+}
+
+
 
 // TODO: Create a function to initialize app
 function init() {}
