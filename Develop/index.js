b1e6bb05644118b46d2890ba9f4470ba14839f25
Gavin <gsantopetro.contact@gmail.com>
2022-10-12 11:33:04 -0500

    got inquirer to ask questions

--- Develop/index.js
+++ Develop/index.js
@@ -12,15 +12,65 @@ const questions = [
     'What are the steps to install this project?',
     'What is an example for usage of this project?',
     'Who do you list in your credits?',
-    'What type of license does this project have?'
+    'What type of license does this project have?',
 
 ];
 
-// TODO: Create a function to write README file
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your project title?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'What is your project motivation?',
+      name: 'motivation',
+    },
+    {
+      type: 'input',
+      message: 'What problems does this project solve?',
+      name: 'problems_solved',
+    },
+    {
+        type: 'input',
+        message: 'What did you learn from this project?',
+        name: 'lessons_learned',
+    },
+    {
+      type: 'input',
+      message: 'What are the steps to install this project?',
+      name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What is an example for usage of this project?',
+        name: 'use_case',
+    },
+    {
+        type: 'input',
+        message: 'Who do you list in your credits?',
+        name: 'credits',
+    },
+    {
+        type: 'input',
+        message: 'What type of license does this project have?',
+        name: 'confirm',
+    },
+  ])
+//   .then((response) =>
+    // response.confirm === response.password
+    //   ? console.log('Success!')
+    //   : console.log('You forgot your password already?!')
+//   );
+
+
 function writeToFile(fileName, data) {
 
-    fs.appendFile('log.md', `${process.argv[2]}\n`, (err) =>
-    // TODO: Describe how this ternary operator works
+    fs.appendFile('README.md', `${process.argv[2]}\n`, (err) =>
+
     err ? console.error(err) : console.log('Commit logged!')
     );
 
