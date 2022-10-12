0e781fcc4e77b37d3a58bd84d5264529ec7aa862
Gavin <gsantopetro.contact@gmail.com>
2022-10-12 10:56:04 -0500

    added array of questions and installed inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,18 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is your project title?',
+    'What is your project motivation?',
+    'What problems does this project solve?',
+    'What did you learn from this project?',
+
+    'What are the steps to install this project?',
+    'What is an example for usage of this project?',
+    'Who do you list in your credits?',
+    'What type of license does this project have?'
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
